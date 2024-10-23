a4bc319c1f78acbfd43ca28adbff1e3158e68dd8
Lemon <m13421884632@163.com>
2024-10-23 15:32:25 +0800

    view修改

---
+++ model/src/main/java/com/tu/mall/mapper/view/AttributeViewMapper.java
@@ -0,0 +1,11 @@
+package com.tu.mall.mapper.view;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tu.mall.entity.view.AttributeView;
+
+/**
+ * @author JiFeiYe
+ * @since 2024/10/23
+ */
+public interface AttributeViewMapper extends BaseMapper<AttributeView> {
+}
