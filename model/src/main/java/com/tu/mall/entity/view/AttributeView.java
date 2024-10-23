7dbde8658416e53a38c7f5aa17b4e1da8d16be6b
Lemon <m13421884632@163.com>
2024-10-23 15:19:26 +0800

    view修改

--- model/src/main/java/com/tu/mall/entity/view/AttributeView.java
+++ model/src/main/java/com/tu/mall/entity/view/AttributeView.java
@@ -1,6 +1,5 @@
 package com.tu.mall.entity.view;
 
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,20 +9,19 @@ import java.io.Serializable;
 
 /**
  * @author JiFeiYe
- * @since 2024/10/18
+ * @since 2024/10/23
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@TableName("category_view")
-public class CategoryView implements Serializable {
+@TableName("attribute_view")
+public class AttributeView implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private Long category1Id;
-    private String category1Name;
-    private Long category2Id;
-    private String category2Name;
-    private Long category3Id;
-    private String category3Name;
+    private Long attrId;
+    private String attrName;
+    private Long categoryId; // 三级
+    private Long attrValueId;
+    private String attrValueName;
 }
