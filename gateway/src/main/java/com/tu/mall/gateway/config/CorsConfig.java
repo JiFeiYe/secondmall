f0e21ff65c31bf3096a8bd9478f2e1a54535e977
Lemon <m13421884632@163.com>
2024-12-03 16:08:17 +0800

    修改CORS配置。

--- gateway/src/main/java/com/tu/mall/gateway/config/CorsConfig.java
+++ gateway/src/main/java/com/tu/mall/gateway/config/CorsConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author JiFeiYe
  * @since 2024/10/14
@@ -13,14 +16,20 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsConfig {
 
-    @Bean
+//    @Bean
     public CorsWebFilter corsWebFilter() {
         //  在此定义规则：
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedHeader("*");        // 设置请求头
         corsConfiguration.addAllowedMethod("*");        // 设置请求方法
-        corsConfiguration.addAllowedOrigin("*");        // 设置请求域名
-//        corsConfiguration.setAllowCredentials(true);    // 允许携带cookie
+        // 添加多个允许的源
+        List<String> allowedOrigins = Arrays.asList(
+                "http://47.115.130.230",
+                "http://127.0.0.1",
+                "http://localhost"
+        );
+        corsConfiguration.setAllowedOrigins(allowedOrigins);        // 设置请求域名
+        corsConfiguration.setAllowCredentials(true);    // 允许携带cookie
 
         UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
         corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
